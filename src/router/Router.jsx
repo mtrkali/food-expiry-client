10dd9f8e609faa27a78d04d2405639d0a3399a6d
mtrkali <tarakfeni95@gmail.com>
2025-09-08 06:22:10 +0600

    setup basic setup and react-router

---
+++ src/router/Router.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { createBrowserRouter } from 'react-router';
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <h1>hellow bangladaesh</h1>
+    }
+])
+
+export default router;
